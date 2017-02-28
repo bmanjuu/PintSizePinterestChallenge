9c83a4dd041360b18ad2e4598b64be8d4c6a9201
bmanjuu <bettyofung@gmail.com>
2017-02-28 11:35:11 -0500

    installed pod and added .gitignore

---
+++ PintSizePinterestChallenge/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  PintSizePinterestChallenge
+//
+//  Created by Betty Fung on 2/28/17.
+//  Copyright © 2017 Betty Fung. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+    
+}
