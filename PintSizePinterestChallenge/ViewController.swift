a6866c0cc4b5acf58d0c5cad98fdf1e6b9279135
bmanjuu <bettyofung@gmail.com>
2017-02-28 11:31:59 -0500

    first commit

---
+++ PintSizePinterestChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PintSizePinterestChallenge
+//
+//  Created by Betty Fung on 2/28/17.
+//  Copyright © 2017 Betty Fung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
