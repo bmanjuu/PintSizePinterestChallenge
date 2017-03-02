768a20c167943e73e35d306583f11b3201447114
bmanjuu <bettyofung@gmail.com>
2017-03-01 22:36:25 -0500

    setting up to persist data

---
+++ PintSizePinterestChallenge/PinterestUserDataStore.swift
@@ -0,0 +1,14 @@
+//
+//  PinterestUserDataStore.swift
+//  PintSizePinterestChallenge
+//
+//  Created by Betty Fung on 3/1/17.
+//  Copyright © 2017 Betty Fung. All rights reserved.
+//
+
+import Foundation
+import PinterstSDK
+
+class PinterestUserDataStore {
+    //persist user and associated board/pin data so only need to authenticate/retrieve information once  
+}
