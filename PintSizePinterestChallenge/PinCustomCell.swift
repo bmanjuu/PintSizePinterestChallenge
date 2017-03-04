ca145d68e36cd612781615b76f8b995020416a79
bmanjuu <bettyofung@gmail.com>
2017-03-04 15:59:27 -0500

    got images to appear before all the errors and bugs occurred, the only bug that is persisting is the last few pins only displaying in 1 column instead of 2

--- PintSizePinterestChallenge/PinCustomCell.swift
+++ PintSizePinterestChallenge/PinCustomCell.swift
@@ -15,10 +15,14 @@ class PinCustomCell: UICollectionViewCell {
     @IBOutlet weak var descriptionTitle: UILabel!
     @IBOutlet weak var descriptionText: UILabel!
     
+    /*
     override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
         super.apply(layoutAttributes)
         if let attributes = layoutAttributes as? PinterestCustomLayoutAttributes {
             //pinImageHeightLayoutConstraint.constant = attributes.photoHeight
         }
     }
+     when this bit of code is active for setting the height of the photo, the images don't show up b/c it conflicts with the height of the photo that we set through autolayout by making the height of the imageview the same as that of the cell. 
+     so, we are automatically doing what this function wants to do through autolayout without actually having to call this function.
+ */
 }
